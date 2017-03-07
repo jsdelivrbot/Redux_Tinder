fe231c0a54c48ef7d3fbf579fee12b3bd0ddd8de
Ruffeng <ruben.suet@gmail.com>
2017-03-07 17:01:41 +0100

    Adding creators. I have no idea what i'm doing right now...

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,5 +1,7 @@
 import React,{Component} from "react";
 import { connect } from 'react-redux';
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
 
 class BookList extends Component{
 
@@ -27,5 +29,12 @@ function mapStateToProps(state){
     books: state.books
   }
 }
+// Anything returned from this function will end up as  props
+// on the BookList container
+function mapDispatchToProps(dispatch){
+  // Whenever selectBook is called , the result should be passed
+  // to all of our reducers
+  return bindActionCreators({ selectBook: selectBook}, dispatch);
+}
 
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps,mapDispatchToProps)(BookList);
